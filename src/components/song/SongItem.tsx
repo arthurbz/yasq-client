b5c5b63049a7b9f97fcc19079eb90b365e1538cf
arthurbz <arthurbziero@gmail.com>
2023-05-30 22:05:56 -0300

    isPlaying prop to highlight current playing song

--- src/components/song/SongItem.tsx
+++ src/components/song/SongItem.tsx
@@ -1,19 +1,25 @@
 import { Row, Col, Typography } from "antd"
+import { theme } from "../../themes/AppThemes"
 import { Song } from "../../types/Song"
 import AlbumCover from "./AlbumCover"
 
 interface SongProps {
     song: Song
+    isPlaying?: boolean
 }
 
-function SongItem({ song }: SongProps) {
+function SongItem({ song, isPlaying }: SongProps) {
     const { name, artist, thumbnail } = song
 
     return (
         <Row
             wrap={false}
             gutter={16}
-            style={{ padding: "8px 16px 8px 16px", width: "100%" }}
+            style={{
+                width: "100%",
+                border: isPlaying ? `3px ${theme.token?.colorPrimary} solid` : "",
+                borderRadius: 8
+            }}
         >
             <AlbumCover
                 thumbnail={thumbnail}
